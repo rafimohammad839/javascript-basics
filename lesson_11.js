de9667cb72b986b1fd9fb525cddeb945d78c0de8
Elyashkhan <elyashkhan2000@gmail.com>
2024-07-03 12:15:33 +0530

    getSeason

--- lesson_11.js
+++ lesson_11.js
@@ -75,7 +75,49 @@
 
 function getSeason(month) {
   
+
+switch (month) {
+  case 12:
+    console.log("December : Winter");
+    break;
+  case 1:
+    console.log("January : Winter");
+    break;
+  case 2:
+    console.log("February : Winter");
+    break;
+  case 3:
+    console.log("March : Spring");
+    break;
+  case 4:
+    console.log("April : Spring");
+    break;
+  case 5:
+    console.log("May : Spring");
+    break;
+  case 6:
+    console.log("June : Summer");
+    break;
+    case 7:
+    console.log("July : Summer");
+    break;
+  case 8:
+    console.log("August : Summer");
+    break;
+  case 9:
+    console.log("September : Fall");
+    break;
+  case 10:
+    console.log("October : Fall");
+    break;
+    case 11:
+      console.log("November : Fall");
+      break;
+  default:
+    console.log("Invalid Month!");
 }
 
-let month = 9; // September
+}
+
+let month = 8; // September
 getSeason(month); // Output: Fall
