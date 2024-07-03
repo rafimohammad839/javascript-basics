6bff5922e0678881841bcc4865155e9d8d168338
Elyashkhan <elyashkhan2000@gmail.com>
2024-07-03 10:22:45 +0530

    Edt functuon

--- lesson_10.js
+++ lesson_10.js
@@ -14,34 +14,45 @@
 
 // Template Literals: Template literals are identified by ` character
 
-const person = {
-    name: "Munna",
-    age: 60,
-    profession: "Plumber",
-    destination: "Dubai",
-    working: true
-  }
+// const person = {
+//     name: "Munna",
+//     age: 60,
+//     profession: "Plumber",
+//     destination: "Dubai",
+//     working: true
+//   }
   
-  // Munna is my friend who is 60 years old and is currently working but is desperate to move to Dubai
+//   // Munna is my friend who is 60 years old and is currently working but is desperate to move to Dubai
   
-  console.log(person.name + " is my friend who is " + person.age + " years old and is currently " +  (person.working ? "working" : "not working") + " but is desperate to move to " + person.destination);
+//   console.log(person.name + " is my friend who is " + person.age + " years old and is currently " +  (person.working ? "working" : "not working") + " but is desperate to move to " + person.destination);
   
   // console.log(`${person.name} is my friend who is ${person.age} years old and is currently ${person.working ? "working" : "not working"} but is desperate to move to ${person.destination}`);
   
   // Question #1: Create an object namely company. Fields are: name, location, revenue. Create a function that takes in company object and returns a template string: eg., Cognizant is based in New Jersey and has a revenue of $100B
   
-const company = {
+ const company = {
     name: "Cognizant",
     location: "New Jersey",
-    revenue: "$100B",
-    
-}
+    revenue: "$100B",    
+};
+    function getCompanyInformation (company){
+        return `${company.name} is based in ${company.location} and has a revenue of ${company.revenue}`;
+    }
 
 
-  console.log(giveCompanyDescription());
+  console.log(getCompanyInformation(company)); 
   
-  
-  
+  const Family = {
+    fmialyleader: 'Munna',
+    femaleleader: 'Wife of Munna',
+    totalCildoOfMunna: '3 bache hai',
+  };
+
+  function getFamilyInformation (Family){
+    return `${Family.fmialyleader} us ke  bibi ke name ${Family.femaleleader} in dono ke  ${Family.totalCildoOfMunna}`;
+  }
+
+  console.log(getFamilyInformation (Family));  
   
   
   
