de5c0de7b53473743ff60435b8c2600789f9cd18
Mohammad Rafi <mohammad.rafi@tothenew.com>
2024-07-03 11:14:53 +0530

    Added a new file: lesson_11.js

---
+++ lesson_11.js
@@ -0,0 +1,81 @@
+// Switch statement
+// Switch statement is similar to if-else ladder. It saves us a lot of if-else code.
+
+// let a = 40;
+
+// switch (a) {
+//   case 10:
+//     console.log('Good Morning!');
+//     break;
+//   case 20:
+//     console.log('Good afternoon!');
+//     break;
+//   case 30:
+//     console.log('Good evening!');
+//     break;
+//   case 5:
+//     console.log('Bonnuit'); // Good night!
+//     break;
+//   case 40:
+//     console.log('Adios, amigos!'); // Goodbye, friends!
+//     break;
+//   case 50:
+//     console.log('Buenos Dias, amigo!'); // Good morning, friend!
+//     break;
+//   default:
+//     console.log('None of the above conditions matches!');
+// }
+
+// Control will come here, out of the switch statement if you add a break statement.
+
+// let day = 2;
+
+// switch (day) {
+//   case 1:
+//     console.log("Monday");
+//     break;
+//   case 2:
+//     console.log("Tuesday");
+//     break;
+//   case 3:
+//     console.log("Wednesday");
+//     break;
+//   case 4:
+//     console.log("Thursday");
+//     break;
+//   case 5:
+//     console.log("Friday");
+//     break;
+//   case 6:
+//     console.log("Saturday");
+//     break;
+//   case 7:
+//     console.log("Sunday");
+//     break;
+//   default:
+//     console.log("Invalid day!");
+// }
+
+// Question #1: Create a function namely getSeason which will accept a month and print to console the season the month falls in. Use switch statement to do this.
+// Timer: 25 minutes
+/* 
+  12 -> December : Winter
+  1 -> January : Winter
+  2 -> February : Winter
+  3 -> March : Spring
+  4 -> April : Spring
+  5 -> May : Spring
+  6 -> June : Summer
+  7 -> July : Summer
+  8 -> August : Summer
+  9 -> September : Fall
+  10 -> October : Fall
+  11 -> November : Fall
+*/
+
+function getSeason(month) {
+  
+}
+
+let month = 9; // September
+getSeason(month); // Output: Fall
